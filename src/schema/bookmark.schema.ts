19ed47df656d84e3d5de93ca569b496e6a8d63fb
Rahul Raj <rahulraz1308@gmail.com>
2023-07-04 08:17:50 +0530

    updated bookmark servive

--- src/schema/bookmark.schema.ts
+++ src/schema/bookmark.schema.ts
@@ -4,77 +4,100 @@ import { Tag, CreateTagInput } from './tag.schema';
 
 @ObjectType()
 export class Bookmark {
+  readonly _id: string;
 
-    readonly _id: string;
+  @Field(() => ID)
+  @prop()
+  id: string;
 
-    @Field(() => ID)
-    @prop()
-    id: string;
-  
-    @Field(()  => String, { nullable: true })
-    @prop()
-    ogTitle: string;
+  @Field(() => String, { nullable: true })
+  @prop()
+  ogTitle: string;
 
-    @Field(() => String, { nullable: true })
-    @prop()
-    ogImg: string;
+  @Field(() => String, { nullable: true })
+  @prop()
+  ogImg: string;
 
-    @Field(()  => String, { nullable: true })
-    @prop()
-    ogDescription: string;
-    
-    @Field(()=> String)
-    @prop()
-    ogUrl: string;
-    
-    @Field(()=> String, { nullable: true })
-    @prop()
-    ogSiteName: string;
+  @Field(() => String, { nullable: true })
+  @prop()
+  ogDescription: string;
 
-    @Field(() => [Tag],{defaultValue:[]})
-    @prop()
-    tags: Tag[];
+  @Field(() => String)
+  @prop()
+  ogUrl: string;
 
-    @Field(() => String)
-    @prop({required: true})
-    userId: string;
+  @Field(() => String, { nullable: true })
+  @prop()
+  ogSiteName: string;
 
-    @Field(() => String)
-    @prop({required: true})
-    hexCode: string;
+  @Field(() => String, { nullable: true })
+  @prop()
+  ogIcon?: string;
+
+  @Field(() => String, { nullable: true })
+  @prop()
+  ogType?: string;
+
+  @Field(() => [Tag], { defaultValue: [] })
+  @prop()
+  tags: Tag[];
+
+  @Field(() => String)
+  @prop({ required: true })
+  userId: string;
+
+  @Field(() => String, { nullable: true })
+  @prop()
+  hexCode: string;
+
+  @Field(() => Boolean)
+  @prop({ default: false })
+  isPreviewAvailable: boolean;
+
+  @Field(() => Number)
+  @prop()
+  createdAt: number;
+
+  @Field(() => Number)
+  @prop()
+  updatedAt: number;
 }
 
 @ObjectType()
 export class GenerateLinkData {
- 
-  @Field(() => String,{nullable: true})
+  @Field(() => String, { nullable: true })
   ogImg?: string;
 
-  @Field(() => String, {nullable: true})
+  @Field(() => String, { nullable: true })
   ogTitle?: string;
 
-  @Field(() => String, {nullable: true})
+  @Field(() => String, { nullable: true })
   ogUrl?: string;
 
-  @Field(() => String, {nullable: true})
+  @Field(() => String, { nullable: true })
   ogSiteName: string;
 
-  @Field(() => String, {nullable: true})
+  @Field(() => String, { nullable: true })
   ogDescription?: string;
-  
+
+  @Field(() => String, { nullable: true })
+  ogIcon?: string;
+
+  @Field(() => String, { nullable: true })
+  ogType?: string;
 }
 
 @InputType()
-export class CreateBookmarkInput {
+export class UpdateBookmarkInput {
   @Field(() => String)
   id: string;
 
   @Field(() => String, { nullable: true })
   ogTitle: string;
-  
+
   @Field(() => String, { nullable: true })
   ogDescription: string;
-  
+
   @Field(() => String)
   ogUrl: string;
 
@@ -84,18 +107,24 @@ export class CreateBookmarkInput {
   @Field(() => String, { nullable: true })
   ogImg: string;
 
-  @Field(()=> [CreateTagInput],{nullable: true})
+  @Field(() => [CreateTagInput], { nullable: true })
   tags: CreateTagInput[];
 
-  @Field(()=> String)
+  @Field(() => String, { nullable: true })
   hexCode: string;
 
+  @Field(() => Number)
+  @prop()
+  updatedAt: number;
 }
 
 @InputType()
 export class GenerateLinkPreviewData {
-  @Field(()=> String)
+  @Field(() => String)
   url: string;
+
+  @Field(() => String)
+  id: string;
 }
 
 @InputType()
